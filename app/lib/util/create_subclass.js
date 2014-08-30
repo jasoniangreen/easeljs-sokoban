2236e21c4c46b8f16ae382e410f22063983cb794
Jason Green <jasoniangreen@gmail.com>
2014-08-30 20:59:47 +0100

    Removed old code.

--- app/lib/util/create_subclass.js
+++ app/lib/util/create_subclass.js
@@ -5,10 +5,7 @@ module.exports = createSubclass;
 function createSubclass(Superclass, name, methods) {
     var Subclass;
 
-    //Subclass = function () { this.initialize.apply(this, arguments) };
     eval('Subclass = function ' + name + '(){ this.initialize.apply(this, arguments) }');
-
-    Subclass.prototype = new Superclass();
     Subclass.prototype = Object.create(Superclass.prototype);
 
     for (var key in methods) {
